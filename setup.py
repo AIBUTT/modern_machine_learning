51f66fcd3e3c23dfb3ac837759de1d344c625f82
aibutt <aibutt@ualberta.ca>
2023-05-25 17:51:22 -0600

    First commit after initializing CookieCutter-Data-Science structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Machine Learning with CookieCutter-Data-Science',
+    author='Aatif Butt',
+    license='MIT',
+)
